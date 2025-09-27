6b2105774d1e473e81271b1cd7254a687f07af47
Meet-hybrid <philipmichaelonyekachi@gmail.com>
2025-09-27 12:04:30 +0100

    Nokia

--- Gateone/TaskFive.java
+++ Gateone/TaskFive.java
@@ -1,16 +1,14 @@
 public class TaskFive {
     public static void main(String[] args) {
 	int counter = 1;
-	int four = 0;
-	int eight = 0;
+	int count = 1;
 	while (counter <= 10) {
-	if(counter % 4 == 0 )
-	four += counter;
-	if(four == 4)
-	eight += counter;
-	if(eight == 8)
+		if(counter % 4 == 0 )
+			for(count= 1;count<=5;count++){
 
-	System.out.println(four + eight);
+				System.out.print( counter);
+				}
+		
 	
 	counter++;
 	}
